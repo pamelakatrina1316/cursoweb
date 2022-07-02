c182fafde3095f67524fe48539c083f6203a2fa8
pam <105122579+pamelakatrina1316@users.noreply.github.com>
2022-07-02 16:37:23 -0300

    hoje

--- jogodavelha/jogodavelha.js
+++ jogodavelha/jogodavelha.js
@@ -1,9 +1,13 @@
-const board= document.querySelector("[data-board]");
 const cellElements= document.querySelectorAll("[data-cell]");
+const board= document.querySelector("[data-board]");
+const winningMessageTextElement= document.querySelector(
+    "[data-winning-message-text]"
+);
 const winningMessage= document.querySelector("[data-winning-message]");
-const winningMessageTextElement= document.querySelector("[data-winning-message-text]");
 const restartButton= document.querySelector("[data-restart-button]");
+
 let isCircleTurn;
+
 const winningCombinations=[
     [0,1,2],
     [3,4,5],
@@ -15,41 +19,41 @@ const winningCombinations=[
     [2,4,6],
 ];
 const startGame= () => {
-    isCircleTurn=false;
+    isCircleTurn =false;
+
+
     for (const cell of cellElements) {
       cell.classList.remove("circle");
       cell.classList.remove("x");
       cell.removeEventListener("click", handleClick);
-      cell.addEventListener("click", handleClick, {once: true});
-    };
+      cell.addEventListener("click", handleClick, { once: true}
+      );
+    }
     setBoardHoverClass();
     winningMessage.classList.remove("show-winning-message");
 };
-const endGame = (isDraw)=>{
+const endGame = (isDraw)=> {
     if(isDraw){
         winningMessageTextElement.innerText="Empate!";
-    } else{
-        winningMessageTextElement.innerHTML="isCircleTurn"
+    } else {
+        winningMessageTextElement.innerText=isCircleTurn
         ?"O venceu!"
         :"X venceu!";
-    };
+    }
     winningMessage.classList.add("show-winning-message");
 };
 
-const checkForWin = (currentPlayer)=>{
+const checkForWin = (currentPlayer)=> {
     return winningCombinations.some ((combination)=>{
         return combination.every((index)=>{
             return cellElements[index].classList.contains(currentPlayer);
-        }
-        );
-    }
-    );
+        });
+    });
 };
 const checkForDraw=()=>{
     return[...cellElements].every((cell) => {
         return cell.classList.contains("x") || cell.classList.contains("circle");
-    }
-    );
+    });
 };
 const placeMark = (cell, classToAdd)=>{
     cell.classList.add(classToAdd);
@@ -57,25 +61,31 @@ const placeMark = (cell, classToAdd)=>{
 const setBoardHoverClass=()=>{
     board.classList.remove("circle");
     board.classList.remove("x");
-    if ("isCircleTurn"){
+
+    if (isCircleTurn){
         board.classList.add("circle");
-    }else{
+    } else {
         board.classList.add("x");
     }
 };
 const swapTurns=()=>{
     isCircleTurn= !isCircleTurn;
+
     setBoardHoverClass();
 };
 const handleClick = (e) => {
     //Colocar a marca (X ou Circulo)
     const cell= e.target;
-    const classList= "isCircleTurn" ? "circle" : "x";
+    const classToAdd= isCircleTurn ? "circle" : "x";
+
     placeMark(cell,classToAdd);
+
     //Verificar por vitória;
     const isWin=checkForWin (classToAdd);
+
     //Verificar por empate
     const isDraw=checkForDraw();
+
     if (isWin) {
         endGame(false);
     }else if (isDraw) {
